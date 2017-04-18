545d05fb2f0cee6f2b83dfefc941dd204c047711
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-04-17 21:10:25 -0500

    Added support to store a PID file

--- FileManager.java
+++ FileManager.java
@@ -11,8 +11,13 @@ public class FileManager
 		this.fileName = _fileName;
 	}
 	
-	public void save_array(double [][]process_signal, double [][]control_signal)
+	
+	public void save_pid_signals(double [][]setpoint, double [][]signal, double [][]control)
 	{
+		/*
+		 * Este método genera una archivo con cuatro columnas que corresponden a:
+		 *    datos_tiempo     datos_setpoint     datos_control     datos_salida_planta
+		 * */
 		BufferedWriter bufferedWriter = null;
 		FileWriter fileWriter = null;
 		PrintWriter printWriter = null;
@@ -21,9 +26,8 @@ public class FileManager
 			fileWriter = new FileWriter(this.fileName, true);
 			bufferedWriter = new BufferedWriter(fileWriter);
 			printWriter = new PrintWriter(bufferedWriter);
-			// formato del archivo ( time_signal, process_signal, control_signal )
-			for(int i = 0; i < process_signal[0].length; i++)
-				bufferedWriter.write(String.format("%f\t%f\t%f\n", process_signal[0][i], process_signal[1][i], control_signal[1][i]));
+			for(int i = 0; i < signal[0].length; i++)                  //time        //setpoint     //control      //plant's output    
+				bufferedWriter.write(String.format("%f\t%f\t%f\t%f\n", signal[0][i], setpoint[1][i], control[1][i], signal[1][i]));
 				
 		}
 		catch(IOException e)
@@ -45,6 +49,48 @@ public class FileManager
 			{
 				
 			}
+		} //finally
+	}// save_pid_signals
+	
+	
+	public void save_array(double [][]process_signal, double [][]stimuli)
+	{
+		/*
+		 * Este método genera una archivo con tres columnas que corresponden a:
+		 *  datos_tiempo    datos_señal_salida_planta     datos_señal_entrada_planta
+		 * 
+		 * */
+		BufferedWriter bufferedWriter = null;
+		FileWriter fileWriter = null;
+		PrintWriter printWriter = null;
+		try
+		{
+			fileWriter = new FileWriter(this.fileName, true);
+			bufferedWriter = new BufferedWriter(fileWriter);
+			printWriter = new PrintWriter(bufferedWriter);
+			for(int i = 0; i < process_signal[0].length; i++)       //time data           //plant's output       //stimuli signal (e.g. step)
+				bufferedWriter.write(String.format("%f\t%f\t%f\n", process_signal[0][i], process_signal[1][i], stimuli[1][i]));
+				
 		}
-	}
-}
+		catch(IOException e)
+		{
+			System.out.println("Error escribiendo el archivo");
+		}
+		finally
+		{
+			try
+			{
+				if(bufferedWriter != null)
+				{
+					bufferedWriter.close();
+					printWriter.close();
+					fileWriter.close();
+				}
+			}
+			catch (IOException e)
+			{
+				
+			}
+		} //finally
+	}//save_array
+} // class FileManager
