1c7b42db94e6f994dc9fc8689a096da4c83b46ad
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-04-17 20:11:00 -0500

    Actualizada la interfaz del server para permitir el ingreso del los parámetros de PID del usuario, aún no se ha modificado el backend

--- Window.java
+++ Window.java
@@ -1,5 +1,4 @@
 import javax.swing.JFrame;
-
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -14,6 +13,7 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+
 public class Window extends JFrame
 {
 	private static final long serialVersionUID = 1L;
@@ -24,7 +24,9 @@ public class Window extends JFrame
 	public JTextField txt_port;
 	public JTextField txt_setpoint;
 	public JTextField txt_sample_time;
-	public JTextField txt_step_time;
+	public JTextField txt_kp;
+	public JTextField txt_ki;
+	public JTextField txt_kd;
 	public JButton btn_server_init;
 	public JButton btn_server_stop;
 	
@@ -41,20 +43,26 @@ public class Window extends JFrame
 		JLabel lbl_param_1 = new JLabel("Port Number");
 		JLabel lbl_param_2 = new JLabel("Setpoint (v)");
 		JLabel lbl_param_3 = new JLabel("Sample Time (s)");
-		JLabel lbl_param_4 = new JLabel("Step Time (s)");
+		JLabel lbl_param_4 = new JLabel("Kp");
+		JLabel lbl_param_5 = new JLabel("Ki");
+		JLabel lbl_param_6 = new JLabel("Kd");
 		txt_port 		= new JTextField("16");
 		txt_setpoint    = new JTextField("1");
 		txt_sample_time = new JTextField("0.5");
-		txt_step_time   = new JTextField("8");
+		txt_kp   = new JTextField("");
+		txt_ki   = new JTextField("");
+		txt_kd   = new JTextField("");
 		txt_port.setHorizontalAlignment(JTextField.CENTER);
 		txt_setpoint.setHorizontalAlignment(JTextField.CENTER);
 		txt_sample_time.setHorizontalAlignment(JTextField.CENTER);
-		txt_step_time.setHorizontalAlignment(JTextField.CENTER);
+		txt_kp.setHorizontalAlignment(JTextField.CENTER);
+		txt_ki.setHorizontalAlignment(JTextField.CENTER);
+		txt_kd.setHorizontalAlignment(JTextField.CENTER);
 		btn_server_init = new JButton("Start Acquisition");
 		btn_server_stop	= new JButton("Save Data");
 		btn_server_init.setActionCommand("start");
 		btn_server_stop.setActionCommand("stop");
-		parametersPanel.setLayout(new GridLayout(5, 2,10,10));
+		parametersPanel.setLayout(new GridLayout(8, 2,10,10));
 		parametersPanel.add(lbl_param_1);
 		parametersPanel.add(txt_port);
 		parametersPanel.add(lbl_param_2);
@@ -62,7 +70,11 @@ public class Window extends JFrame
 		parametersPanel.add(lbl_param_3);
 		parametersPanel.add(txt_sample_time);
 		parametersPanel.add(lbl_param_4);
-		parametersPanel.add(txt_step_time);
+		parametersPanel.add(txt_kp);
+		parametersPanel.add(lbl_param_5);
+		parametersPanel.add(txt_ki);
+		parametersPanel.add(lbl_param_6);
+		parametersPanel.add(txt_kd);
 		parametersPanel.add(btn_server_init);
 		parametersPanel.add(btn_server_stop);
 		mainPanel.add(parametersPanel, BorderLayout.EAST);
