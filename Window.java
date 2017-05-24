9b915438b55550112425e1c0a4fa685a0812ec23
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-05-23 23:07:32 -0500

    Interfaz modificada para solo lectura, el usuario solo puede ingresar la IP a la que se desea conectar e inicia un proceso de comunicación y captura de datos

--- Window.java
+++ Window.java
@@ -14,6 +14,7 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 
+
 public class Window extends JFrame
 {
 	private static final long serialVersionUID = 1L;
@@ -25,23 +26,13 @@ public class Window extends JFrame
 	final XYSeries setpoint_II;
 	final XYSeriesCollection collectionData;
 	final JFreeChart display;
-	public JTextField txt_port;
-	public JTextField txt_setpoint_I;
-	public JTextField txt_sample_time_I;
-	public JTextField txt_setpoint_II;
-	public JTextField txt_sample_time_II;
-	public JTextField txt_kp_T;
-	public JTextField txt_ki_T;
-	public JTextField txt_kd_T;
-	public JTextField txt_kp_F;
-	public JTextField txt_ki_F;
-	public JTextField txt_kd_F;
-	public JButton btn_server_init;
+	public JTextField txt_server_ip;
+	public JButton btn_server_start;
 	public JButton btn_server_stop;
 	
 	public Window()
 	{
-		this.setTitle("Real Time Control App");
+		this.setTitle("Real Time Control Viewer App");
 		this.setBounds(600,440,800,550);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel mainPanel = new JPanel();
@@ -49,75 +40,16 @@ public class Window extends JFrame
 		
 		/*Parameters panel*/
 		JPanel parametersPanel = new JPanel();
-		JLabel lbl_param_1  = new JLabel("Port Number");
-		JLabel lbl_param_2  = new JLabel("Setpoint (v)");
-		JLabel lbl_param_2_2  = new JLabel("Setpoint (v)");
-		JLabel lbl_param_3  = new JLabel("Sample Time (s)");
-		JLabel lbl_param_3_2  = new JLabel("Sample Time (s)");
-		JLabel lbl_param_4  = new JLabel("<HTML><u>PID Controller I</u></HTML>");
-		JLabel lbl_param_5  = new JLabel("Kp");
-		JLabel lbl_param_6  = new JLabel("Ki");
-		JLabel lbl_param_7  = new JLabel("Kd");
-		JLabel lbl_param_8  = new JLabel("<HTML><u>PID Controller II</u></HTML>");
-		JLabel lbl_param_9  = new JLabel("Kp");
-		JLabel lbl_param_10 = new JLabel("Ki");
-		JLabel lbl_param_11 = new JLabel("Kd");
-		JLabel lbl_dummi_1    = new JLabel("        ");
-		JLabel lbl_dummi_2    = new JLabel("        ");
-		txt_port 		= new JTextField("16");
-		txt_setpoint_I    = new JTextField("1");
-		txt_sample_time_I = new JTextField("0.5");
-		txt_setpoint_II    = new JTextField("1");
-		txt_sample_time_II = new JTextField("0.5");
-		txt_kp_T   = new JTextField("0");
-		txt_ki_T   = new JTextField("0");
-		txt_kd_T   = new JTextField("0");
-		txt_kp_F   = new JTextField("0");
-		txt_ki_F   = new JTextField("0");
-		txt_kd_F   = new JTextField("0");
-		txt_port.setHorizontalAlignment(JTextField.CENTER);
-		txt_setpoint_I.setHorizontalAlignment(JTextField.CENTER);
-		txt_sample_time_I.setHorizontalAlignment(JTextField.CENTER);
-		txt_setpoint_II.setHorizontalAlignment(JTextField.CENTER);
-		txt_sample_time_II.setHorizontalAlignment(JTextField.CENTER);
-		txt_kp_T.setHorizontalAlignment(JTextField.CENTER);
-		txt_ki_T.setHorizontalAlignment(JTextField.CENTER);
-		txt_kd_T.setHorizontalAlignment(JTextField.CENTER);
-		txt_kp_F.setHorizontalAlignment(JTextField.CENTER);
-		txt_ki_F.setHorizontalAlignment(JTextField.CENTER);
-		txt_kd_F.setHorizontalAlignment(JTextField.CENTER);
-		btn_server_init = new JButton("Start Control");
-		btn_server_stop	= new JButton("Stop Control");
+		JLabel lbl_server_ip    = new JLabel("Server IP");
+		txt_server_ip = new JTextField("192.168.");
+		btn_server_init = new JButton("Start Session");
+		btn_server_stop	= new JButton("Stop Session");
 		btn_server_init.setActionCommand("start");
 		btn_server_stop.setActionCommand("stop");
-		parametersPanel.setLayout(new GridLayout(14, 2,10,10));
-		parametersPanel.add(lbl_param_1);
-		parametersPanel.add(txt_port);
-		parametersPanel.add(lbl_param_4);
-		parametersPanel.add(lbl_dummi_1);
-		parametersPanel.add(lbl_param_2);
-		parametersPanel.add(txt_setpoint_I);
-		parametersPanel.add(lbl_param_3);
-		parametersPanel.add(txt_sample_time_I);
-		parametersPanel.add(lbl_param_5);
-		parametersPanel.add(txt_kp_T);
-		parametersPanel.add(lbl_param_6);
-		parametersPanel.add(txt_ki_T);
-		parametersPanel.add(lbl_param_7);
-		parametersPanel.add(txt_kd_T);
-		parametersPanel.add(lbl_param_8);
-		parametersPanel.add(lbl_dummi_2);
-		parametersPanel.add(lbl_param_2_2);
-		parametersPanel.add(txt_setpoint_II);
-		parametersPanel.add(lbl_param_3_2);
-		parametersPanel.add(txt_sample_time_II);
-		parametersPanel.add(lbl_param_9);
-		parametersPanel.add(txt_kp_F);
-		parametersPanel.add(lbl_param_10);
-		parametersPanel.add(txt_ki_F);
-		parametersPanel.add(lbl_param_11);
-		parametersPanel.add(txt_kd_F);
-		parametersPanel.add(btn_server_init);
+		parametersPanel.setLayout(new GridLayout(2, 2,10,10));
+		parametersPanel.add(lbl_server_ip);
+		parametersPanel.add(txt_server_ip);
+		parametersPanel.add(btn_server_start);
 		parametersPanel.add(btn_server_stop);
 		mainPanel.add(parametersPanel, BorderLayout.EAST);
 		this.getContentPane().add(mainPanel, BorderLayout.EAST);
