6ccd9baab5d3df5dd138a27ec9993b968effafe8
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-05-23 23:06:40 -0500

    Actualización de seguridad

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main implements ActionListener
 	public static void main(String []args)
 	{
 		Window root = new Window();
-		root.btn_server_init.addActionListener(new Main());
+		root.btn_server_start.addActionListener(new Main());
 		root.btn_server_stop.addActionListener(new Main());
 		myThread = new ThreadServer(root);
 	}
@@ -16,12 +16,12 @@ public class Main implements ActionListener
 	{
 		if(e.getActionCommand().equals("start"))
 		{
-			System.out.println("Iniciando el servidor");
+			System.out.println("Iniciando el Cliente");
 			myThread.start();
 		}
 		else if(e.getActionCommand().equals("stop"))
 		{
-			System.out.println("Parando el servidor");
+			System.out.println("Finalizando el Cliente");
 			myThread.terminate();
 			
 		}
